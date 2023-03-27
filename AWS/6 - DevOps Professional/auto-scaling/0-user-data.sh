22d2176ed0b1875e184c5b3288a13e31b4766615
Olzhas Tolegen <turatam@hotmail.com>
2023-03-27 15:52:38 +0600

    Additional folder structure change

--- AWS/6 - DevOps Professional/auto-scaling/0-user-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Please make sure to launch Amazon Linux 2
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
-echo "Healthy" > /var/www/html/health.html
