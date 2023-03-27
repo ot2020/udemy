269b50a44ada2dcf8b38cb4ef0a0fa08807435c2
Olzhas Tolegen <turatam@hotmail.com>
2023-03-27 11:10:38 +0600

    Changed folders structure for easy navigation

--- 6 - DevOps Professional/cloudwatch/cloudwatch-custom-metrics.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Publish a custom metric
-aws cloudwatch put-metric-data --metric-name FunnyMetric --namespace Custom --value 1243 --dimensions InstanceId=1-23456789,InstanceType=m1.small --profile aws-devops --region eu-west-1
